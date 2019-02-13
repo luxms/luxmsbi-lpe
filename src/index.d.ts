dc758418f0099537a5bf4a722dbb202498fb5bc6
esix <esix@live.ru>
2019-02-13 12:42:05 +0300

    Fixed d.ts to be compatible to es6 modules

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,9 @@
-declare module 'lpe' {
+export function eval_lisp(prog: any): any;
 
+export function parse(lpeExpression: string): any;
+
+
+declare module 'lpe' {
   export function eval_lisp(prog: any): any;
 
   export function parse(lpeExpression: string): any;
