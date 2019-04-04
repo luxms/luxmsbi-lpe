87e6dbc33f9f2e75ec4734bb1c5f857fabc5ae6c
esix <esix@live.ru>
2019-04-04 12:38:49 +0300

    Deparse -> operator

--- dist/lpe.js
+++ dist/lpe.js
@@ -1,4 +1,4 @@
-/** [LPE]  Version: 1.0.0 - 2019/04/04 12:38:08 */ 
+/** [LPE]  Version: 1.0.0 - 2019/04/04 12:38:46 */ 
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
