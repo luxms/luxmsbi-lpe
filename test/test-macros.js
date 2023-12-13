cc5af8bc13d4fe3902ae3a6bf6f40c22d13242f1
dpetrov <dpetrov@luxms.com>
2023-12-13 15:12:47 +0300

    tests: add clickhouse sql_macros tests

--- test/test-macros.js
+++ test/test-macros.js
@@ -5,7 +5,15 @@ var lpe = require('../dist/lpe');
 
 describe('SQL Macros', function() {
 
-    it('should eval cast to INT', function() {
+    it('should eval cast to INT using postgresql style without specified db', function() {
+      assert.equal( lpe.eval_sql_macros(
+         lpe.parse("cast(i, INT)")),
+    `    ALTER COLUMN "i" SET DATA TYPE INT
+    USING utils.safe_convert_to_int("i", NULL::INT)`
+            );
+    });
+
+    it('should eval cast to INT (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(i, INT)"),
                {"_target_database": "postgresql"}),
@@ -14,7 +22,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval cast to INT with default "NULL"', function() {
+    it('should eval cast to INT with default "NULL" (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("cast(i, INT, NULL)"),
                 {"_target_database": "postgresql"}),
@@ -23,7 +31,7 @@ describe('SQL Macros', function() {
              );
     });
 
-    it('should eval cast to INT with default 0', function() {
+    it('should eval cast to INT with default 0 (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("cast(i, INT, 0)"),
                 {"_target_database": "postgresql"}),
@@ -32,7 +40,7 @@ describe('SQL Macros', function() {
              );
     });
 
-    it('should eval castWithExpr to INT', function() {
+    it('should eval castWithExpr to INT (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("castWithExpr(i, INT, left(regexp('(\\\\d+)'), 1) )"),
                 {"_target_database": "postgresql"}),
@@ -41,7 +49,7 @@ describe('SQL Macros', function() {
              );
     });
 
-    it('should eval castWithExpr to INT with dot', function() {
+    it('should eval castWithExpr to INT with dot (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("castWithExpr(i, INT, regexp('(\\\\d+)').left(1) )"),
                 {"_target_database": "postgresql"}),
@@ -59,7 +67,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval cast to FLOAT with default 0', function() {
+    it('should eval cast to FLOAT with default 0 (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(f_col, FLOAT, 0)"),
                {"_target_database": "postgresql"}),
@@ -68,7 +76,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval cast to DOUBLE', function() {
+    it('should eval cast to DOUBLE (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(f_col, DOUBLE)"),
                {"_target_database": "postgresql"}),
@@ -77,7 +85,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval cast to DOUBLE with default 0', function() {
+    it('should eval cast to DOUBLE with default 0 (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(f_col, DOUBLE, 0)"),
                {"_target_database": "postgresql"}),
@@ -86,7 +94,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval castWithExpr to DOUBLE', function() {
+    it('should eval castWithExpr to DOUBLE (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("castWithExpr(f_col, DOUBLE, left(regexp('(\\\\d+)'), 1) )"),
                 {"_target_database": "postgresql"}),
@@ -95,7 +103,7 @@ describe('SQL Macros', function() {
              );
     });
 
-    it('should eval cast to STRING', function() {
+    it('should eval cast to STRING (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(t, STRING)"),
                {"_target_database": "postgresql"}),
@@ -104,7 +112,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval cast to STRING with default "TEST"', function() {
+    it('should eval cast to STRING with default "TEST" (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(t, STRING, 'TEST')"),
                {"_target_database": "postgresql"}),
@@ -113,7 +121,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval castWithExpr to STRING', function() {
+    it('should eval castWithExpr to STRING (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("castWithExpr(t, STRING, left(regexp('(\\\\d+)'), 1) )"),
                 {"_target_database": "postgresql"}),
@@ -122,7 +130,7 @@ describe('SQL Macros', function() {
              );
     });
 
-    it('should eval cast to DATE', function() {
+    it('should eval cast to DATE (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(d, DATE)"),
                {"_target_database": "postgresql"}),
@@ -131,7 +139,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval cast to DATE with default 2000-01-01', function() {
+    it('should eval cast to DATE with default 2000-01-01 (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(d, DATE, '2000-01-01')"),
                {"_target_database": "postgresql"}),
@@ -140,7 +148,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval castWithExpr to DATE with to_date', function() {
+    it('should eval castWithExpr to DATE with to_date (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("castWithExpr(i, DATE, to_date('DD.mm.YYYY') )"),
                 {"_target_database": "postgresql"}),
@@ -149,7 +157,7 @@ describe('SQL Macros', function() {
              );
     });
 
-    it('should eval castWithExpr to DATE with to_date and default', function() {
+    it('should eval castWithExpr to DATE with to_date and default (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("castWithExpr(i, DATE, to_date('DD.mm.YYYY'), '2000-01-01' )"),
                 {"_target_database": "postgresql"}),
@@ -158,7 +166,7 @@ describe('SQL Macros', function() {
              );
     });
 
-    it('should eval cast to DATETIME', function() {
+    it('should eval cast to DATETIME (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(d, DATETIME)"),
                {"_target_database": "postgresql"}),
@@ -167,7 +175,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval cast to DATETIME with default 2000-01-01', function() {
+    it('should eval cast to DATETIME with default 2000-01-01 (postgresql)', function() {
       assert.equal( lpe.eval_sql_macros(
          lpe.parse("cast(d, DATETIME, '2000-01-01')"),
                {"_target_database": "postgresql"}),
@@ -176,7 +184,7 @@ describe('SQL Macros', function() {
             );
     });
 
-    it('should eval castWithExpr to DATETIME with to_datetime', function() {
+    it('should eval castWithExpr to DATETIME with to_datetime (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("castWithExpr(i, DATETIME, to_datetime('DD.mm.YYYY hh:mi'))"),
                 {"_target_database": "postgresql"}),
@@ -185,13 +193,61 @@ describe('SQL Macros', function() {
              );
     });
 
-    it('should eval castWithExpr to DATETIME with to_datetime and default', function() {
+    it('should eval castWithExpr to DATETIME with to_datetime and default (postgresql)', function() {
        assert.equal( lpe.eval_sql_macros(
           lpe.parse("castWithExpr(i, DATETIME, to_datetime('DD.mm.YYYY hh:mi'), '2000-01-01 00:01' )"),
                 {"_target_database": "postgresql"}),
     `    ALTER COLUMN "i" SET DATA TYPE TIMESTAMP
     USING utils.safe_convert_to_timestamp("i", 'DD.mm.YYYY hh:mi', '2000-01-01 00:01'::TIMESTAMP)`
              );
+    });
+
+    it('should eval cast to INT (clickhouse)', function() {
+      assert.equal( lpe.eval_sql_macros(
+         lpe.parse("cast(i, INT)"),
+               {"_target_database": "clickhouse"}),
+    `    MODIFY COLUMN "i" Nullable(Int64)`
+            );
+    });
+
+    it('should eval cast to FLOAT (clickhouse)', function() {
+      assert.equal( lpe.eval_sql_macros(
+         lpe.parse("cast(f_col, FLOAT)"),
+               {"_target_database": "clickhouse"}),
+    `    MODIFY COLUMN "f_col" Nullable(Float64)`
+            );
+    });
+
+    it('should eval cast to DOUBLE (clickhouse)', function() {
+      assert.equal( lpe.eval_sql_macros(
+         lpe.parse("cast(f_col, DOUBLE)"),
+               {"_target_database": "clickhouse"}),
+    `    MODIFY COLUMN "f_col" Nullable(Float64)`
+            );
+    });
+
+    it('should eval cast to STRING (clickhouse)', function() {
+      assert.equal( lpe.eval_sql_macros(
+         lpe.parse("cast(t, STRING)"),
+               {"_target_database": "clickhouse"}),
+    `    MODIFY COLUMN "t" Nullable(String)`
+            );
+    });
+
+    it('should eval cast to DATE (clickhouse)', function() {
+      assert.equal( lpe.eval_sql_macros(
+         lpe.parse("cast(d, DATE)"),
+               {"_target_database": "clickhouse"}),
+    `    MODIFY COLUMN "d" Nullable(Date)`
+            );
+    });
+
+    it('should eval cast to DATETIME (clickhouse)', function() {
+      assert.equal( lpe.eval_sql_macros(
+         lpe.parse("cast(d, DATETIME)"),
+               {"_target_database": "clickhouse"}),
+    `    MODIFY COLUMN "d" Nullable(DateTime)`
+            );
     });
 
     //console.log(JSON.stringify(lpe.parse("castWithExpr(i, INT, left(regexp('(\\d+\\n)'), 1) )")))
