0099a4a01a291797ac771a6011f249550211a67c
esix <esix@live.ru>
2025-01-15 20:50:59 +0300

    1.4.1

--- src/index.d.ts
+++ src/index.d.ts
@@ -15,6 +15,7 @@ export function isFunction(v: any): boolean;
 export function isNumber(v: any): boolean;
 export function makeSF(v: any): any;
 export const STDLIB: any;
+export function unbox(args: any[], unboxFn: (values: any[]) => any, streamAdapter?: any): any;
 
 
 declare module 'lpe' {
@@ -25,6 +26,8 @@ declare module 'lpe' {
   export function eval_lisp(prog: any, ctx?: any): any;
 
   export function eval_lpe(prog: string, ctx?: any): any;
+
+  export function unbox(args: any[], unboxFn: (values: any[]) => any, streamAdapter?: any): any;
 
   export const LPESyntaxError: any;
 
