fe7d13b5c17c7e54f5bfbe653a512bf17b0d3452
Dmitry Zvyagin <dzvyagin@luxms.com>
2026-03-16 13:23:40 +0300

    fix: now '.-' function not evaluate property name as varable

--- src/lisp.js
+++ src/lisp.js
@@ -466,11 +466,11 @@ const SPECIAL_FORMS = {                                                         
      *          property({a = 1}, "b", 2) => 2 ## Значение объекта: { a:1, b:2 }
      * @category Работа с переменными | 12
      */                                                          // get or set attribute
-    let [obj, propertyName, value] = ast.map(a => EVAL(a, ctx, options));
-    // hack
-    if (propertyName === undefined && isString(ast[1])) {                                           // string propertyName tried to evaluate in rs context
-      propertyName = ast[1];
-    }
+    let [obj, propertyName, value] = [
+      EVAL(ast[0], ctx, options),
+      isString(ast[1]) ? ast[1] : EVAL(ast[1], ctx, options),
+      EVAL(ast[2], ctx, options),
+    ];
     return unbox(
         [obj, propertyName, value],
         ([obj, propertyName, value]) => {
